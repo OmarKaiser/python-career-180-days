8e7a7f30fd710da129387ef1cfaebffeed248d2c
Omar Kaiser Mahin <omarkaisar80@gmail.com>
2026-08-11 18:01:47 +0600

    Day 03: Python loops and iteration

---
+++ Day03/loops.py
@@ -0,0 +1,23 @@
+""" numb = int(input("Enter a number: "))
+
+for number in range(1,11):
+    print(numb, "x", number, "=", numb * number) """
+
+""" count = 10
+
+while count >= 1:
+    print(count)
+    count = count - 1
+     """
+
+""" for number in range(1,11):
+    print(number)
+    if number == 7:
+        break
+     """
+numb = int(input("Enter a Number: "))
+result = 1
+for number in range(1, numb+1):
+    result = result * number
+
+print(result)
